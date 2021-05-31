1754a47de8017af459e1de5d0cbf0af34be8c7c3
Mariana Lima <limatainer@gmail.com>
2021-05-31 22:56:54 +0100

    update

---
+++ backend/src/main/java/com/crudclients/crudclient/services/ClientService.java
@@ -0,0 +1,84 @@
+package com.crudclients.crudclient.services;
+
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.crudclients.crudclient.dto.ClientDTO;
+import com.crudclients.crudclient.entities.Client;
+import com.crudclients.crudclient.repositories.ClientRepository;
+import com.crudclients.crudclient.services.exceptions.DatabaseException;
+import com.crudclients.crudclient.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class ClientService {
+
+	@Autowired
+	private ClientRepository repository;
+
+
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Client> list = repository.findAll(pageRequest);
+		return list.map(x -> new ClientDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity, entity.getCategories());
+	}
+
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+		Product entity = new Product();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+		try {
+			Product entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ProductDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity Violation");
+		}
+	}
+
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setDate(dto.getDate());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setPrice(dto.getPrice());
+
+		entity.getCategories().clear();
+		for (CategoryDTO catDto : dto.getCategories()) {
+			Category category = categoryRepository.getOne(catDto.getId());
+			entity.getCategories().add(category);
+		}
+	}
+}
---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO tb_client (name, cpf, date, income, birth_Date, children) VALUE ('Marilia Luiza', '88776678445', TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', '8000', '1993-02-22', '0');
